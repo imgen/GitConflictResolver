44bf3eb3e4537a6a6c78dd406bff06abcbfc9d33
Imgen <imgen@hotmail.com>
2019-09-07 11:48:19 +0800

    Simplify code down to 98 lines

--- src/Program.cs
+++ src/Program.cs
@@ -8,16 +8,14 @@ namespace GitConflictResolver
 {
     class Program
     {
-        private const string ConflictHeader = "<<<<<<<", 
-                             ConflictSeparator = "=======", 
-                             ConflictFooter = ">>>>>>>";
+        private const string Header = "<<<<<<<",  Separator = "=======", Footer = ">>>>>>>";
         private static readonly string[] ResolveModes = {"mt", "tm", "m", "t", "none"};
         static async Task<int> Main(string[] args)
         {
             if (args.Length < 2)
             {
-                Console.Error.WriteLine("Please provide enough parameters");
-                Console.Error.WriteLine(@"Usage: GitConflictResolver FILEPATH [RESOLVEMODE]
+                Console.Error.WriteLine(@"Please provide enough parameters
+Usage: GitConflictResolver FILEPATH [RESOLVEMODE]
 FILEPATH: The path of the file to process
 RESOLVEMODE: Indicate how to resolve the conflicted sections. Possible values: 
 mt - Put mine before theirs
@@ -27,33 +25,31 @@ t - Only keep theirs
 none - Keep none");
                 return -1;
             }
-
-            var file = args[0];
-            var mode = args[1].ToLowerInvariant();
+            var (file, mode) = (args[0], args[1].ToLowerInvariant());
             if (!ResolveModes.Contains(mode))
             {
-                Console.WriteLine($"Invalid resolve mode {mode}, possible values: mt, tm, m, t, none");
+                Console.WriteLine($"Invalid resolve mode {mode}, possible values: {string.Join(',', ResolveModes)}");
+                return -1;
             }
 
             var lines = await File.ReadAllLinesAsync(file);
-            var conflicts = new List<Conflict>();
-            var context = new List<string>();
+            var (conflicts, context) = (new List<Conflict>(), new List<string>());
             using (var walker = lines.OfType<string>().GetEnumerator())
             {
                 string GetNextLine() => walker.MoveNext() ? walker.Current : null;
                 string line; 
                 while ((line = GetNextLine()) != null)
                 {
-                    if (line.StartsWith(ConflictHeader, StringComparison.Ordinal))
+                    if (line.StartsWith(Header, StringComparison.Ordinal))
                     {
                         var mine = new List<string>();
-                        while (!(line = GetNextLine()).StartsWith(ConflictSeparator, StringComparison.Ordinal))
+                        while (!(line = GetNextLine()).StartsWith(Separator, StringComparison.Ordinal))
                         {
                             mine.Add(line);
                         }
 
                         var theirs = new List<string>();
-                        while (!(line = GetNextLine()).StartsWith(ConflictFooter, StringComparison.Ordinal))
+                        while (!(line = GetNextLine()).StartsWith(Footer, StringComparison.Ordinal))
                         {
                             theirs.Add(line);
                         }
@@ -78,57 +74,24 @@ none - Keep none");
             var resolvedLines = conflicts.SelectMany(c => c.Resolve(mode)).ToArray();
             var text = string.Join(Environment.NewLine, resolvedLines);
             await File.WriteAllTextAsync(file, text);
-
             return 0;
         }
     }
-
     public class Conflict
     {
         public List<string> Mine, Theirs, Before, After;
-
-        public Conflict(List<string> mine,
-                        List<string> theirs,
-                        List<string> before)
-        {
-            Mine = mine;
-            Theirs = theirs;
-            Before = before;
-            After = new List<string>();
-        }
-
-        public string[] Resolve(string mode)
+        public Conflict(List<string> mine, List<string> theirs, List<string> before) =>
+            (Mine, Theirs, Before, After) = (mine, theirs, before, new List<string>());
+        public IEnumerable<string> Resolve(string mode)
         {
             switch (mode)
             {
-                case "mt":
-                    return Before
-                        .Concat(Mine)
-                        .Concat(Theirs)
-                        .Concat(After)
-                        .ToArray();
-                case "tm":
-                    return Before
-                        .Concat(Theirs)
-                        .Concat(Mine)
-                        .Concat(After)
-                        .ToArray();
-                case "m":
-                    return Before
-                        .Concat(Mine)
-                        .Concat(After)
-                        .ToArray();
-                case "t":
-                    return Before
-                        .Concat(Theirs)
-                        .Concat(After)
-                        .ToArray();
-                case "none":
-                    return Before
-                        .Concat(After)
-                        .ToArray();
-                default:
-                    throw new ApplicationException($"Invalid resolve mode {mode}");
+                case "mt": return Before.Concat(Mine).Concat(Theirs).Concat(After);
+                case "tm": return Before.Concat(Theirs).Concat(Mine).Concat(After);
+                case "m": return Before.Concat(Mine).Concat(After);
+                case "t": return Before.Concat(Theirs).Concat(After);
+                case "none": return Before.Concat(After).ToArray();
+                default: throw new ApplicationException($"Invalid resolve mode {mode}");
             }
         }
     }
