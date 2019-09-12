b0ba8d1e5d64692ebf68999025cb34485f5ef8e4
billshu <bill.shu@gravitysupplychain.com>
2019-09-12 18:46:58 +0800

    Less succinct but more understandable

--- src/Program.cs
+++ src/Program.cs
@@ -32,11 +32,12 @@ none - Keep none");
             var resolvedLines = (await File.ReadAllLinesAsync(file)).SelectMany(line =>
             {
                 (var newLines, var prefix) = (new List<string>(), line.Substring(0, Math.Min(line.Length, ">>>>>>>".Length)));
-                curMode = prefix == "<<<<<<<"? 'm' : prefix == "======="? 't' : prefix == ">>>>>>>"? ' ' : curMode;
-                if (prefix == ">>>>>>>")
-                    (newLines, changes['m'], changes['t']) = (mode.SelectMany(c => changes.ContainsKey(c) ? changes[c] : new List<string>()).ToList(), 
-                                new List<string>(), new List<string>());
-                else if (prefix != "<<<<<<<" && prefix != "=======")
+                if (prefix == "<<<<<<<" || prefix == "=======")
+                    curMode = prefix == "<<<<<<<"? 'm' : 't';
+                else if (prefix == ">>>>>>>") { 
+                    newLines = mode.SelectMany(c => changes.ContainsKey(c) ? changes[c] : new List<string>()).ToList();
+                    (changes['m'], changes['t'], curMode) = (new List<string>(), new List<string>(), ' ');
+                }  else
                     (changes.ContainsKey(curMode)? changes[curMode] : newLines).Add(line);
                 return newLines;
             }).ToArray();
