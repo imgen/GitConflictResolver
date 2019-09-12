093587bb7d37e4b1625e6eed723fc1e445bde6c1
billshu <bill.shu@gravitysupplychain.com>
2019-09-12 11:25:59 +0800

    The best way yet

--- src/Program.cs
+++ src/Program.cs
@@ -8,8 +8,6 @@ namespace GitConflictResolver
 {
     class Program
     {
-        private const string Header = "<<<<<<<",  Separator = "=======", Footer = ">>>>>>>";
-        private static readonly string[] ResolveModes = {"mt", "tm", "m", "t", "none"};
         static async Task<int> Main(string[] args)
         {
             if (args.Length < 2)
@@ -26,53 +24,30 @@ none - Keep none");
                 return -1;
             }
             var (file, mode) = (args[0], args[1].ToLowerInvariant());
-            if (!ResolveModes.Contains(mode))
+            if (!new []{"mt", "tm", "m", "t", "none"}.Contains(mode))
             {
-                Console.WriteLine($"Invalid resolve mode {mode}, possible values: {string.Join(",", ResolveModes)}");
+                Console.WriteLine($"Invalid resolve mode {mode}");
                 return -1;
             }
-
-            (string[] lines, string line, int i, var conflicts) = (await File.ReadAllLinesAsync(file), null, -1, new List<Conflict>());
-            string GetNextLine() => ++i < lines.Length ? lines[i] : null;
-            IEnumerable<string> TakeNextLineUntilStartsWith(string prefix)
+            var (changes, currentPrefix) = (new Dictionary<char, List<string>> { ['m'] = new List<string>(), ['t'] = new List<string>() }, "");
+            var resolvedLines = (await File.ReadAllLinesAsync(file)).SelectMany(line =>
             {
-                while ((line = GetNextLine()) != null && !line.StartsWith(prefix, StringComparison.Ordinal))
-                    yield return line;
-            }
-            for(var context = TakeNextLineUntilStartsWith(Header).ToList(); line != null;)
-            {
-                var mine = TakeNextLineUntilStartsWith(Separator).ToList();
-                var theirs = TakeNextLineUntilStartsWith(Footer).ToList();
-                conflicts.Add(new Conflict(mine, theirs, conflicts.Any()? null : context,
-                    context = TakeNextLineUntilStartsWith(Header).ToList()));
-            }
-            if (!conflicts.Any())
-            {
-                Console.WriteLine($"There is no conflicts in file {file}");
-                return 0;
-            }
-            var resolvedLines = conflicts.SelectMany(c => c.Resolve(mode)).ToArray();
-            var text = string.Join(Environment.NewLine, resolvedLines);
-            await File.WriteAllTextAsync(file, text);
+                var (newLines, prefix) = (new List<string>(), line.Substring(0, Math.Min(line.Length, ">>>>>>>".Length)));
+                if (prefix == "<<<<<<<" || prefix == "=======")
+                    currentPrefix = prefix;
+                else if (prefix == ">>>>>>>")
+                {
+                    newLines = mode.SelectMany(c => changes.ContainsKey(c) ? changes[c] : new List<string>()).ToList();
+                    (currentPrefix, changes['m'], changes['t']) = ("", new List<string>(), new List<string>());
+                }
+                else if (currentPrefix == "")
+                    newLines.Add(line);
+                else
+                    (currentPrefix == "<<<<<<<" ? changes['m'] : changes['t']).Add(line);
+                return newLines;
+            }).ToArray();
+            await File.WriteAllTextAsync(file, string.Join(Environment.NewLine, resolvedLines));
             return 0;
-        }
-    }
-    public class Conflict
-    {
-        public List<string> Before, Mine, Theirs, After;
-        public Conflict(List<string> mine, List<string> theirs, List<string> before = null, List<string> after = null) =>
-            (Before, Mine, Theirs, After) = (before?? new List<string>(), mine, theirs, after?? new List<string>());
-        public IEnumerable<string> Resolve(string mode)
-        {
-            switch (mode)
-            {
-                case "mt": return Before.Concat(Mine).Concat(Theirs).Concat(After);
-                case "tm": return Before.Concat(Theirs).Concat(Mine).Concat(After);
-                case "m": return Before.Concat(Mine).Concat(After);
-                case "t": return Before.Concat(Theirs).Concat(After);
-                case "none": return Before.Concat(After).ToArray();
-                default: throw new ApplicationException($"Invalid resolve mode {mode}");
-            }
         }
     }
 }
